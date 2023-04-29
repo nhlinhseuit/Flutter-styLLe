1d3ba3d45009832dbf29e9647e6d9e50eef4dae5
LAPTOP-9Q9UCI39\ASUS <21522289@gm.uit.edu.vn>
2023-04-29 23:36:30 +0700

    bottom nav - grid view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,7 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-import '../constants/enums.dart';
-import '../constants/routes.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:stylle/pages/home_page_delegated.dart';
 import '../services/auth/auth_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,114 +14,101 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = AuthService.firebase().currentUser;
 
+  int _selectedIndex = 0;
+
+  final tabs = const [
+    Center(child: Text('HOME')),
+    Center(child: Text('ADD')),
+    Center(child: Text('FAVORITE')),
+    Center(child: Text('PROFILE')),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.values[0]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.black,
-        title: Text(
-          'styLLe',
-          style: GoogleFonts.allura(
-            color: Theme.of(context).colorScheme.primary,
-            fontSize: 56,
-          ),),
-        actions: [
-          PopupMenuButton<MenuAction>(
-            itemBuilder: (context) {
-              return const [
-                PopupMenuItem<MenuAction>(
-                  value: MenuAction.logout,
-                  child: Text('Logout'),
-                )
-              ];
-            },
-            onSelected: (value) async {
-              switch(value) {
-                case MenuAction.logout:
-                  final confirmLogout = await showLogOutDialog(context);
-                  if (confirmLogout) {
-                    await AuthService.firebase().logout();
-                    if (mounted) {
-                      Navigator.of(context).pushNamedAndRemoveUntil(loginRoute, (_) => false,);
-                    }
-                  }
-                  break;
-              }
-            }
-          )
-        ],
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(32),
-        child: Column(
-          children: [
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Hi there  ",
-                    style: GoogleFonts.abhayaLibre(
-                      color: Colors.black,
-                      fontSize: 16
-                    )
-                  ),
-                  const WidgetSpan(
-                    child: Icon(Icons.waving_hand_outlined, size: 16),
-                  ),
-                ],
+      body: const HomePageDelegated(),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: Offset(0, 2), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 14),
+          child: GNav(
+            backgroundColor: Colors.white,
+            color: Colors.white,
+            activeColor: Colors.white,
+            // tabBackgroundColor: const Color.fromRGBO(255, 191, 202, 100),
+            tabBackgroundColor: Colors.pink[300]!,
+            padding: const EdgeInsets.all(7),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                iconColor: Color.fromRGBO(255, 191, 202, 100),
+                text: 'Home',
+                gap: 6,
               ),
-            )   
-        ]),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Theme.of(context).colorScheme.primary,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
+              GButton(
+                icon: Icons.add_box_rounded,
+                iconColor: Color.fromRGBO(255, 191, 202, 100),
+                text: 'Add',
+                gap: 6,
+              ),
+              GButton(
+                icon: Icons.heart_broken,
+                iconColor: Color.fromRGBO(255, 191, 202, 100),
+                gap: 6,
+                text: 'Favorite',
+              ),
+              GButton(
+                icon: Icons.person_2,
+                iconColor: Color.fromRGBO(252, 191, 202, 100),
+                text: 'Profile',
+                gap: 6,
+              ),
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (value) {
+              setState(() {
+                _selectedIndex = value;
+              });
+            },
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: 'Upload',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_outlined),
-            label: 'Profile',
-          )
-        ],
+        ),
       ),
     );
-  
   }
 }
 
 Future<bool> showLogOutDialog(BuildContext context) {
-
   return showDialog<bool>(
-    context: context, 
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure you want to log out'),
-        actions: [
-          TextButton(
-            onPressed: () {
-            Navigator.of(context).pop(false);
-            }, 
-            child: const Text('Cancel')
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            }, 
-            child: const Text('Log out')),
-        ],
-      );
-  }).then((value) => value ?? false);
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Sign out'),
+          content: const Text('Are you sure you want to log out'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Log out')),
+          ],
+        );
+      }).then((value) => value ?? false);
 }
