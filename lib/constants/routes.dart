948a917102bb0fde32e2c0a9a99c7a1f3a773ee2
LAPTOP-9Q9UCI39\ASUS <21522289@gm.uit.edu.vn>
2023-05-01 15:37:30 +0700

    detail_page

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -4,3 +4,5 @@ const registerRoute = '/register/';
 const homeRoute = '/home/';
 const verifyRoute = '/verify/';
 const forgotPasswordRoute = '/forgotPassword/';
+const detailPageRout = '/detail/';
+const detailDemoRout = '/detaildemo/';
